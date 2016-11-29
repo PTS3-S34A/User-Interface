79549a5ae986017f588b16e0c6b9d27f9466b699
Limpens,Raymond R.H.B. <Ranray@users.noreply.github.com>
2016-11-29 11:29:30 +0100

    Refactor MainMenuFXMLXontroller class. (Commented local Joining Sessions and added Statistics table)

--- Soccar [UI]/src/nl/soccar/ui/fx/controller/MainMenuFXMLController.java
+++ Soccar [UI]/src/nl/soccar/ui/fx/controller/MainMenuFXMLController.java
@@ -1,6 +1,8 @@
 package nl.soccar.ui.fx.controller;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
@@ -13,6 +15,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
@@ -20,9 +23,10 @@ import javafx.scene.control.TableView.TableViewSelectionModel;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.control.cell.PropertyValueFactory;
 import nl.soccar.exception.UIException;
+import nl.soccar.library.Player;
 import nl.soccar.library.Session;
-import nl.soccar.library.SessionController;
-import nl.soccar.library.Soccar;
+import nl.soccar.library.Statistics;
+import nl.soccar.rmi.ClientController;
 import nl.soccar.ui.Main;
 import nl.soccar.ui.fx.FXMLConstants;
 
@@ -49,35 +53,63 @@ public class MainMenuFXMLController implements Initializable {
     @FXML
     private TableView<SessionTableItem> tblSessionList;
     @FXML
-    private TableColumn tbclName;
+    private TableView<StatisticsTableItem> tblStatisticsList;
     @FXML
-    private TableColumn tbclOccupation;
+    private TableColumn tbclSessionRoomName;
     @FXML
-    private TableColumn tbclOwner;
+    private TableColumn tbclSessionOccupation;
     @FXML
-    private TableColumn tbclPassword;
-
-    private SessionController sessionController;
+    private TableColumn tbclSessionHost;
+    @FXML
+    private TableColumn tbclSessionPassword;
+    @FXML
+    private TableColumn tbclStatisticUsername;
+    @FXML
+    private TableColumn tbclStatisticGoals;
+    @FXML
+    private TableColumn tbclStatisticAssists;
+    @FXML
+    private TableColumn tbclStatisticRatio;
+    @FXML
+    private TableColumn tbclStatisticGamesWon;
+    @FXML
+    private TableColumn tbclStatisticGamesEven;
+    @FXML
+    private TableColumn tbclStatisticGamesLost;
+    @FXML
+    private Tab tabSession;
+    @FXML
+    private Tab tabStatistic;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        sessionController = SessionController.getInstance();
 
         // Overwrite the standard placeholder text with an empty String.
         tblSessionList.setPlaceholder(new Label(""));
+        tblStatisticsList.setPlaceholder(new Label(""));
 
-        lblUsername.setText(Soccar.getInstance().getCurrentPlayer().getUsername());
-        lblCar.setText(Soccar.getInstance().getCurrentPlayer().getCarType().toString());
+        Player currentPlayer = ClientController.getInstance().getCurrentPlayer();
+        
+        lblUsername.setText(currentPlayer.getUsername());
+        lblCar.setText(currentPlayer.getCarType().name());
 
         btnLogOut.setOnAction(e -> Main.getInstance().logOut());
         btnCreateRoom.setOnAction(e -> Main.getInstance().setScene(FXMLConstants.LOCATION_CREATE_ROOM));
         btnJoinRoom.setOnAction(e -> joinRoom(tblSessionList.getSelectionModel().getSelectedItem()));
         btnJoinRoom.setDisable(true);
 
-        tbclName.setCellValueFactory(new PropertyValueFactory<>("roomName"));
-        tbclOccupation.setCellValueFactory(new PropertyValueFactory<>("occupancy"));
-        tbclOwner.setCellValueFactory(new PropertyValueFactory<>("hostName"));
-        tbclPassword.setCellValueFactory(new PropertyValueFactory<>("passwordAvailable"));
+        tbclSessionRoomName.setCellValueFactory(new PropertyValueFactory<>("roomName"));
+        tbclSessionOccupation.setCellValueFactory(new PropertyValueFactory<>("occupancy"));
+        tbclSessionHost.setCellValueFactory(new PropertyValueFactory<>("hostName"));
+        tbclSessionPassword.setCellValueFactory(new PropertyValueFactory<>("passwordAvailable"));
+
+        tbclStatisticUsername.setCellValueFactory(new PropertyValueFactory<>("username"));
+        tbclStatisticGoals.setCellValueFactory(new PropertyValueFactory<>("goals"));
+        tbclStatisticAssists.setCellValueFactory(new PropertyValueFactory<>("assists"));
+        tbclStatisticRatio.setCellValueFactory(new PropertyValueFactory<>("ratio"));
+        tbclStatisticGamesWon.setCellValueFactory(new PropertyValueFactory<>("gamesWon"));
+        tbclStatisticGamesEven.setCellValueFactory(new PropertyValueFactory<>("gamesEven"));
+        tbclStatisticGamesLost.setCellValueFactory(new PropertyValueFactory<>("gamesLost"));
 
         tblSessionList.setRowFactory(tv -> {
             TableRow<SessionTableItem> row = new TableRow();
@@ -93,54 +125,64 @@ public class MainMenuFXMLController implements Initializable {
         model.setSelectionMode(SelectionMode.SINGLE);
         model.selectedItemProperty().addListener(l -> btnJoinRoom.setDisable(model.getSelectedItem() == null));
 
-        updateTable();
+        tabStatistic.setOnSelectionChanged(e -> updateSessionTable());
+        tabStatistic.setOnSelectionChanged(e -> updateStatisticTable());
     }
 
-    private void updateTable() {
+    private void updateSessionTable() {
         ObservableList<SessionTableItem> sessionItems = FXCollections.observableArrayList();
-        SessionController.getInstance().getAllSessions().stream().map(SessionTableItem::new).forEach(sessionItems::add);
+        ClientController.getInstance().getAllSessions().stream().map(SessionTableItem::new).forEach(sessionItems::add);
 
         tblSessionList.getItems().clear();
         tblSessionList.getItems().addAll(sessionItems);
     }
 
+    private void updateStatisticTable() {
+        ObservableList<StatisticsTableItem> statisticItems = FXCollections.observableArrayList();
+        
+        List<Statistics> statistics = ClientController.getInstance().getAllStatistics();
+        statistics.stream().map(StatisticsTableItem::new).forEach(statisticItems::add);
+
+        tblStatisticsList.getItems().clear();
+        tblStatisticsList.getItems().addAll(statisticItems);
+    }
+
     private void joinRoom(SessionTableItem selectedRow) {
-        Session selectedSession = selectedRow.getSession();
-        if (selectedSession == null) {
-            // There isn't actually a selected session, we should just do nothing.
-            return;
-        }
-
-        String password = NO_PASSWORD;
-        if (selectedSession.getRoom().passwordAvailable()) {
-            TextInputDialog dialog = new TextInputDialog("Password");
-            dialog.setTitle("Room Locked");
-            dialog.setHeaderText("Room Locked");
-            dialog.setContentText("Enter the room password:");
-
-            Optional<String> result = dialog.showAndWait();
-            if (!result.isPresent()) {
-                // There's no password set, we should just do nothing.
-                return;
-            }
-            
-            password = result.get();
-        }
-
-        try {
-            Soccar.getInstance().getCurrentPlayer().setCurrentSession(sessionController.join(selectedSession, password, Soccar.getInstance().getCurrentPlayer()));
-
-            Main.getInstance().setScene(FXMLConstants.LOCATION_SESSION_VIEW);
-        } catch (UIException e) {
-            LOGGER.log(Level.WARNING, "An error occurred while joining a room.", e);
-
-            Alert alert = new Alert(Alert.AlertType.WARNING);
-            alert.setTitle(e.getTitle());
-            alert.setHeaderText(e.getTitle());
-            alert.setContentText(e.getMessage());
-
-            alert.showAndWait();
-        }
+//        Session selectedSession = selectedRow.getSession();
+//        if (selectedSession == null) {
+//            // There isn't actually a selected session, we should just do nothing.
+//            return;
+//        }
+//
+//        String password = NO_PASSWORD;
+//        if (selectedSession.getRoom().passwordAvailable()) {
+//            TextInputDialog dialog = new TextInputDialog("Password");
+//            dialog.setTitle("Room Locked");
+//            dialog.setHeaderText("Room Locked");
+//            dialog.setContentText("Enter the room password:");
+//
+//            Optional<String> result = dialog.showAndWait();
+//            if (!result.isPresent()) {
+//                // There's no password set, we should just do nothing.
+//                return;
+//            }
+//            password = result.get();
+//        }
+//
+//        try {
+//            // TODO : Implement joining session with sending a message to the Main-Server.
+//            
+//            Main.getInstance().setScene(FXMLConstants.LOCATION_SESSION_VIEW);
+//        } catch (UIException e) {
+//            LOGGER.log(Level.WARNING, "An error occurred while joining a room.", e);
+//
+//            Alert alert = new Alert(Alert.AlertType.WARNING);
+//            alert.setTitle(e.getTitle());
+//            alert.setHeaderText(e.getTitle());
+//            alert.setContentText(e.getMessage());
+//
+//            alert.showAndWait();
+//        }
     }
 
 }
