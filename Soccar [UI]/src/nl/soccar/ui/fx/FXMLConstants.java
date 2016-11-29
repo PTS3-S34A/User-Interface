32c2f8f36585066bc0d8145484b4f29b5d9ceff2
Limpens,Raymond R.H.B. <Ranray@users.noreply.github.com>
2016-11-29 11:25:39 +0100

    Refactor FXMLConstants class. (Added new FXML file locations to the FXMLConstans)

--- Soccar [UI]/src/nl/soccar/ui/fx/FXMLConstants.java
+++ Soccar [UI]/src/nl/soccar/ui/fx/FXMLConstants.java
@@ -1,21 +1,29 @@
 package nl.soccar.ui.fx;
 
 /**
- * This class keeps track of all constants regarding JavaFX. For example the locations of 
- * FXML files.
- * 
+ * This class keeps track of all constants regarding JavaFX. For example the
+ * locations of FXML files.
+ *
  * @author PTS34A
  */
 public final class FXMLConstants {
-    
+
     // Locations of FXML files.
     public static final String LOCATION_MAIN_MENU = "resources/fxml/MainMenuFXML.fxml";
     public static final String LOCATION_LOGIN = "resources/fxml/LoginFXML.fxml";
     public static final String LOCATION_CREATE_ROOM = "resources/fxml/CreateRoomFXML.fxml";
     public static final String LOCATION_SESSION_VIEW = "resources/fxml/SessionViewFXML.fxml";
     public static final String LOCATION_GAME_VIEW = "resources/fxml/GameViewFXML.fxml";
-    
+    public static final String LOCATION_GAME_RESULTS = "resources/fxml/GameResultsFXML.fxml";
+    public static final String LOCATION_REGISTER = "resources/fxml/RegisterFXML.fxml";
+
+    // GUI warnings & description
+    public static final String WARNING_USERNAME_EXISTS = "THIS USERNAME EXISTS";
+    public static final String WARNING_NEW_USERNAME = "REGISTER THIS USERNAME";
+    public static final String BUTTON_USERNAME_EXISTS = "LOGIN";
+    public static final String BUTTON_NEW_USERNAME = "REGISTER";
+
     private FXMLConstants() {
     }
-    
+
 }
