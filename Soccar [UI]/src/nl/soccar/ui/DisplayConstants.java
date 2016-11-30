8f78d4780342144d3b8b345891899bbed716207e
Robin Laugs <Laugslander@users.noreply.github.com>
2016-11-30 18:09:22 +0100

    Moved the map constants to a seperate class file named MapConstants that is located in the Library project.

--- Soccar [UI]/src/nl/soccar/ui/DisplayConstants.java
+++ Soccar [UI]/src/nl/soccar/ui/DisplayConstants.java
@@ -19,32 +19,6 @@ public final class DisplayConstants {
     public static final float SCREEN_HEIGHT = 800.0F;
 
     /**
-     * Ball constant in JBox2D units.
-     */
-    public static final float BALL_RADIUS = 2.5F;
-
-    /**
-     * Car constant in JBox2D units.
-     */
-    public static final float CAR_WIDTH = 3.0F;
-
-    /**
-     * Map constants in JBox2D units.
-     */
-    public static final float MAP_WIDTH = 160.0F;
-    public static final float MAP_HEIGHT = 90.0F;
-    public static final float SCOREBOARD_WIDTH = 10.0F;
-    public static final float SCOREBOARD_HEIGHT = 6.0F;
-    public static final float LINE_WIDTH = 0.2F;
-    public static final float CENTRE_CIRCLE_SIZE = 13.75F;
-    public static final float CENTRE_SPOT_SIZE = 1.5F;
-    public static final float FIELD_MARGIN = 5.0F;
-    public static final float BOX_WIDTH = 12.5F;
-    public static final float BOX_HEIGHT = 32.5F;
-    public static final float GOAL_WIDTH = 3.5F;
-    public static final float GOAL_HEIGHT = BOX_HEIGHT / 1.5F;
-
-    /**
      * Map image location constants.
      */
     public static final String LOCATION_TEXTURE_MOON = "resources/images/moon_texture.jpg";
