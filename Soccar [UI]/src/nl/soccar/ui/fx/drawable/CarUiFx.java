adb0b6efba3383b340ea83d19f039c4cceea8dfa
Limpens,Raymond R.H.B. <Ranray@users.noreply.github.com>
2016-12-08 13:54:58 +0100

    Fixed client movement

--- Soccar [UI]/src/nl/soccar/ui/fx/drawable/CarUiFx.java
+++ Soccar [UI]/src/nl/soccar/ui/fx/drawable/CarUiFx.java
@@ -32,14 +32,13 @@ public class CarUiFx extends PhysicsDrawable<Car, CarPhysics> {
     private static final Font PLAYER_FONT;
     private static final Color COLOR_WHEEL;
 
-
     static {
         COLOR_WHEEL = Color.grayRgb(50);
         PLAYER_FONT = new Font("Arial", 20);
     }
 
     private Image carTexture;
-    
+
     public CarUiFx(GameCanvas canvas, Car car, TeamColour colour) {
         this(canvas, car, new CarPhysics(car, canvas.getGameEngine().getWorld()), colour);
     }
@@ -47,8 +46,8 @@ public class CarUiFx extends PhysicsDrawable<Car, CarPhysics> {
     /**
      * Initiates a new CarUiFx Object using the given parameters.
      *
-     * @param canvas  The canvas on which this Car is placed.
-     * @param car     The model to keep track of.
+     * @param canvas The canvas on which this Car is placed.
+     * @param car The model to keep track of.
      * @param physics The physics-model to keep track of.
      */
     public CarUiFx(GameCanvas canvas, Car car, CarPhysics physics, TeamColour colour) {
@@ -60,19 +59,22 @@ public class CarUiFx extends PhysicsDrawable<Car, CarPhysics> {
     @Override
     public void draw(GraphicsContext context) {
         CarPhysics physics = super.getPhysicsModel();
-        
-        SteerAction steerAction = Keyboard.getSteerAction();
-        HandbrakeAction handbrakeAction = Keyboard.getHandbrakeAction();
-        ThrottleAction throttleAction = Keyboard.getThrottleAction();
-        
-        super.getModel().setSteerAction(steerAction);
-        super.getModel().setHandbrakeAction(handbrakeAction);
-        super.getModel().setThrottleAction(throttleAction);
-        
-        // TODO : Change location of sending this message (Ugly implementation for now)
-        Connection connection = ClientController.getInstance().getCurrentConnection();
-        connection.send(new PlayerMovedMessage(steerAction, handbrakeAction, throttleAction));
-        
+
+        if (ClientController.getInstance().getCurrentPlayer() == super.getModel().getPlayer()) {
+            SteerAction steerAction = Keyboard.getSteerAction();
+            HandbrakeAction handbrakeAction = Keyboard.getHandbrakeAction();
+            ThrottleAction throttleAction = Keyboard.getThrottleAction();
+
+            super.getModel().setSteerAction(steerAction);
+            super.getModel().setHandbrakeAction(handbrakeAction);
+            super.getModel().setThrottleAction(throttleAction);
+
+            // TODO : Change location of sending this message (Ugly implementation for now)
+            Connection connection = ClientController.getInstance().getCurrentConnection();
+            connection.send(new PlayerMovedMessage(steerAction, handbrakeAction, throttleAction));
+
+        }
+
         physics.getWheels().forEach(w -> drawWheel(w, context));
         drawBody(context);
     }
