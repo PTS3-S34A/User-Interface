e4ab5172d304aa1bea33f0b38c7bed15cecfc9a2
bras1223 <l.hermans98@gmail.com>
2016-12-11 23:42:30 +0100

    Deleted duplicate code & fixed user equals username.

--- Soccar [UI]/src/nl/soccar/ui/fx/controller/GameResultsFXMLController.java
+++ Soccar [UI]/src/nl/soccar/ui/fx/controller/GameResultsFXMLController.java
@@ -29,7 +29,7 @@ import nl.soccar.ui.fx.FXMLConstants;
  * @author PTS34A
  */
 public class GameResultsFXMLController implements Initializable {
-
+    
     @FXML
     private Label lblRoomName;
     @FXML
@@ -54,13 +54,16 @@ public class GameResultsFXMLController implements Initializable {
     private TableView<ResultTableItem> tblBlueGoalsList;
     @FXML
     private TableView<ResultTableItem> tblRedGoalsList;
-
+    
     private static final Logger LOGGER = Logger.getLogger(GameResultsFXMLController.class.getSimpleName());
-
+    
     private Session currentSession;
-
+    
     private Player currentPlayer;
-
+    
+    private Room room;
+    private Game game;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         ClientController controller = ClientController.getInstance();
@@ -69,17 +72,17 @@ public class GameResultsFXMLController implements Initializable {
 
         btnLogOut.setOnAction(e -> Main.getInstance().logOut());
         btnExitResults.setOnAction(e -> closeResults());
-
+        
         tbclBlueUsername.setCellValueFactory(new PropertyValueFactory<>("username"));
         tbclRedUsername.setCellValueFactory(new PropertyValueFactory<>("username"));
         tbclBlueGoalsScored.setCellValueFactory(new PropertyValueFactory<>("goalsScored"));
         tbclRedGoalsScored.setCellValueFactory(new PropertyValueFactory<>("goalsScored"));
-
+        
         lblUsername.setText(currentPlayer.getUsername());
         lblCar.setText(currentPlayer.getCarType().toString());
-
+        
         setGameInfo();
-
+        
         Optional<SessionData> session = controller.getAllSessions().stream().filter(s -> s.getRoomName().equals(lblRoomName.getText())).findFirst();
         if (!session.isPresent()) {
             LOGGER.log(Level.WARNING, "An exception occured while getting the SessionData from the Game Server");
@@ -91,42 +94,38 @@ public class GameResultsFXMLController implements Initializable {
      * Method that displays a summary of the game on the GameResults scene.
      */
     public void setGameInfo() {
-        Room room = currentSession.getRoom();
-        Game game = currentSession.getGame();
-
-        for (Player p : room.getTeamBlue().getPlayers()) {
-            int goals = 0;
-            for (Event e : game.getEvents()) {
-                if (e.getType() == EventType.GOAL_BLUE && e.getPlayer().equals(p)) {
-                    goals++;
-                }
-            }
-
-            ResultTableItem r = new ResultTableItem(p.getUsername(), goals);
+        room = currentSession.getRoom();
+        game = currentSession.getGame();
+        
+        for (Player p : room.getTeamBlue().getPlayers()) {       
+            ResultTableItem r = new ResultTableItem(p.getUsername(), countGoalsUser(EventType.GOAL_BLUE, p));
             tblBlueGoalsList.getItems().add(r);
         }
-
+        
         for (Player p : room.getTeamRed().getPlayers()) {
-            int goals = 0;
-            for (Event e : game.getEvents()) {
-                if (e.getType() == EventType.GOAL_BLUE && e.getPlayer().equals(p)) {
-                    goals++;
-                }
-            }
-
-            ResultTableItem r = new ResultTableItem(p.getUsername(), goals);
+            ResultTableItem r = new ResultTableItem(p.getUsername(), countGoalsUser(EventType.GOAL_RED, p));
             tblRedGoalsList.getItems().add(r);
         }
-
+        
         StringBuilder scoreString = new StringBuilder();
         scoreString.append("RED ");
         scoreString.append(game.getEventCountByType(EventType.GOAL_RED));
         scoreString.append(" - ");
         scoreString.append(game.getEventCountByType(EventType.GOAL_BLUE));
         scoreString.append(" BLUE");
-
+        
         lblScoreEnd.setText(scoreString.toString());
         lblRoomName.setText("Game Results - " + room.getName());
+    }
+    
+    private int countGoalsUser(EventType type, Player p) {
+        int goals = 0;
+        for (Event e : game.getEvents()) {
+            if (e.getType() == EventType.GOAL_BLUE && e.getPlayer().getUsername().equals(p.getUsername())) {
+                goals++;
+            }
+        }
+        return goals;
     }
 
     /**
@@ -135,5 +134,5 @@ public class GameResultsFXMLController implements Initializable {
     public void closeResults() {
         Main.getInstance().setScene(FXMLConstants.LOCATION_SESSION_VIEW);
     }
-
+    
 }
