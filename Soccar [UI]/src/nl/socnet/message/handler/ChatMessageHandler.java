07c4f1f98958fa7171c6c3545ff1b5d9b51c10a4
Limpens,Raymond R.H.B. <Ranray@users.noreply.github.com>
2016-12-14 13:11:44 +0100

    Refactor ChatMessageHandler

--- Soccar [UI]/src/nl/socnet/message/handler/ChatMessageHandler.java
+++ Soccar [UI]/src/nl/socnet/message/handler/ChatMessageHandler.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package nl.socnet.message.handler;
 
 import io.netty.buffer.ByteBuf;
@@ -17,7 +12,7 @@ import nl.socnet.message.ChatMessage;
 
 /**
  *
- * @author Raymond
+ * @author PTS34A
  */
 public final class ChatMessageHandler extends MessageHandler<ChatMessage> {
 
