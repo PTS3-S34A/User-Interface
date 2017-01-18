e13992ba6912ed55f4cdf3093e3f583477d5432e
Limpens,Raymond R.H.B. <Ranray@users.noreply.github.com>
2017-01-18 11:44:50 +0100

    Implemented requested changes from reviewer.

--- Soccar [UI]/src/nl/soccar/ui/input/Keyboard.java
+++ Soccar [UI]/src/nl/soccar/ui/input/Keyboard.java
@@ -55,7 +55,7 @@ public final class Keyboard {
     }
     
     // Stores the keys that are being pressed at any time.
-    private final List<KeyCode> PRESSED_KEYS = new ArrayList<>();
+    private final List<KeyCode> pressedKeys = new ArrayList<>();
 
     /**
      * Constructor for initializing the Keyboard object.
@@ -70,8 +70,8 @@ public final class Keyboard {
      * @param code The keycode that needs to be added to the pressedKeys list.
      */
     public void setKeyPressed(KeyCode code) {
-        if (!PRESSED_KEYS.contains(code)) {
-            PRESSED_KEYS.add(0, code); // Prepend to the list, so the last key pressed gets first priority
+        if (!pressedKeys.contains(code)) {
+            pressedKeys.add(0, code); // Prepend to the list, so the last key pressed gets first priority
         }
     }
 
@@ -81,7 +81,7 @@ public final class Keyboard {
      * @param code The keycode that needs to be removed of the pressedKeys list.
      */
     public void setKeyReleased(KeyCode code) {
-        PRESSED_KEYS.remove(code);
+        pressedKeys.remove(code);
     }
 
     /**
@@ -91,7 +91,7 @@ public final class Keyboard {
      */
     ThrottleAction getThrottleAction() {
         // The last pressed key
-        for (KeyCode pressedKey : PRESSED_KEYS) {
+        for (KeyCode pressedKey : pressedKeys) {
 
             if (BOOST.contains(pressedKey)) {
                 return ThrottleAction.BOOST;
@@ -116,7 +116,7 @@ public final class Keyboard {
      */
     SteerAction getSteerAction() {
 
-        for (KeyCode pressedKey : PRESSED_KEYS) {
+        for (KeyCode pressedKey : pressedKeys) {
 
             if (STEER_LEFT.contains(pressedKey)) {
                 return SteerAction.STEER_LEFT;
@@ -137,7 +137,7 @@ public final class Keyboard {
      */
     HandbrakeAction getHandbrakeAction() {
 
-        for (KeyCode pressedKey : PRESSED_KEYS) {
+        for (KeyCode pressedKey : pressedKeys) {
 
             if (HANDBRAKE.contains(pressedKey)) {
                 return HandbrakeAction.ACTIVE;
